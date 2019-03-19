571ce86bff2a8d45e59c44ad5701c9019499b3a9
Pallaviburoju <pallaviburoju@gmail.com>
2019-03-19 19:44:34 +0530

    loan project

--- FlatRegistration(Collections)/src/main/java/com/cpg/frs/utility/OwneridIncorrectException.java
+++
@@ -1,9 +0,0 @@
-package com.cpg.frs.utility;
-
-public class OwneridIncorrectException extends Exception
-{
-
-	public void OwnerIdIncorrectException() {
-		System.out.println("Enter valid owner id");
-	}
-}
